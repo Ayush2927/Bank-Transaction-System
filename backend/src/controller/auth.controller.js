d38a2544d6715dc56d534068e684cf70ab40523b
Ayush Mahapatro <ayushm050405@gmail.com>
2026-06-01 23:00:51 +0530

    feat: implement user login with JWT authentication and password validation

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -30,8 +30,43 @@ async function userRegister(req,res){
             id:user._id,
             email:user.email,
             name:user.name
-        }
+        },token
     });
 }
 
-export {userRegister}
+async function userLogin(req,res){
+    const {email,password} = req.body;
+
+    const user=await User.findOne(
+        {email}
+    ).select("+password")
+
+    if(!user){
+        return res.status(401).json({
+            message:"email or password is invalid"
+        })
+    }
+
+   const isvalidPassword= await user.comparePassword(password);
+
+     if(!isvalidPassword){
+    return res.status(401).json({
+        message:"password is invalid"
+    })
+}
+
+   const token=jwt.sign({userId:user._id},process.env.JWT_SECRET,{expiresIn:"3d"})
+    res.cookie("token",token);
+
+    res.status(200).json({
+        user: {
+            email: user.email,
+            _id: user._id,
+            name: user.name
+        },
+        token
+    })
+
+}
+
+export {userRegister,userLogin}
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,21 +1,12 @@
 import express from "express"
-import { userRegister } from "../controller/auth.controller.js";
+import { userRegister,userLogin } from "../controller/auth.controller.js";
 
 const router=express.Router();
 
+ //Post /api/auth/register
 router.post("/register",userRegister)
 
-
-
-
-
-
-
-
-
-
-
-
-
+//Post api/auth/login
+router.post("/login",userLogin)
 
 export  default router
