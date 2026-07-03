7834b26ab5aaa8cc66fda8f9ea8ff9f0681a449e
Ayush Mahapatro <ayushm050405@gmail.com>
2026-07-03 19:38:11 +0530

    Handled export related mismatch

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -1,4 +1,4 @@
-import { User } from "../models/user.model.js";
+import User from "../models/user.model.js";
 import jwt from "jsonwebtoken"
 import { sendRegistrationMail } from "../services/email.service.js";
 //User register controller
--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,46 +1,46 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs"
 
-const UserSchema= mongoose.Schema({
-    email:{
-        type:String,
-        required:[true,"email is required for creating user"],
-        trim:true,
-        lowercase:true,
-        match:[/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, "Invalid email address"],
-        unique:[true,"Email already exists"]
+const UserSchema = mongoose.Schema({
+    email: {
+        type: String,
+        required: [true, "email is required for creating user"],
+        trim: true,
+        lowercase: true,
+        match: [/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, "Invalid email address"],
+        unique: [true, "Email already exists"]
     },
-    
-    name:{
-        type:String,
-        required:[true,"Name is required for creating an account"],
-        
+
+    name: {
+        type: String,
+        required: [true, "Name is required for creating an account"],
+
     },
-    
-    password:{
-        type:String,
-        required:[true,"password is required for creating an account"],
-        minLength:[6,"password should contain more than 6 characters "],
-        select:false
+
+    password: {
+        type: String,
+        required: [true, "password is required for creating an account"],
+        minLength: [6, "password should contain more than 6 characters "],
+        select: false
     }
 },
-{
-    timestamps:true
-})
+    {
+        timestamps: true
+    })
 
-UserSchema.pre("save", async function(){
-    if(!this.isModified("password")){
+UserSchema.pre("save", async function () {
+    if (!this.isModified("password")) {
         return;
     }
-    const hash=await bcrypt.hash(this.password,10);
-    this.password=hash;
+    const hash = await bcrypt.hash(this.password, 10);
+    this.password = hash;
 });
 
-UserSchema.methods.comparePassword=async function(password){
-    console.log(password,this.password)
-    return await bcrypt.compare(password,this.password);
+UserSchema.methods.comparePassword = async function (password) {
+    console.log(password, this.password)
+    return await bcrypt.compare(password, this.password);
 }
 
-const User=mongoose.model("User",UserSchema)
+const User = mongoose.model("User", UserSchema)
 
-export {User};
+export default User ;
