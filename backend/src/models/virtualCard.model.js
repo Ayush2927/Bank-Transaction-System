b065ffaa5050621c02111869a9007b1c7c911f96
Ayush Mahapatro <ayushm050405@gmail.com>
2026-08-12 00:25:25 +0530

    Set up the database model structure for virtual cards

---
+++ backend/src/models/virtualCard.model.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose"
+
+const virtualCardSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: [true, "Card must belong to a user"],
+        index: true
+    },
+
+    account: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "accountModel",
+        required: [true, "Card must be listed to an account"]
+    },
+
+    cardNumber: {
+        type: String,
+        unique: true,
+        length: 16,
+        required: true
+    },
+
+    cardHolderName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    expiryDate: {
+        type: String,
+        required: true
+    },
+
+    cvv: {
+        type: String,
+        required: true
+    },
+    isFrozen: {
+        type: boolean,
+        default: false
+    },
+    monthlyLimit: {
+        type: Number,
+        default: 1000
+    }
+}, { timestamps: true })
+
+const virtualCardModel = mongoose.model("virtualCardModel", virtualCardSchema)
+
+export { virtualCardModel }
